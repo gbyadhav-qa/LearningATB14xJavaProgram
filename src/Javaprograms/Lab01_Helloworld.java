8869b5df4641053373b0025ee1f3b9a62907cee2
Ganga <gbyadhav@gmail.com>
2025-09-13 08:43:12 +0530

    Demo test case

---
+++ src/Javaprograms/Lab01_Helloworld.java
@@ -0,0 +1,10 @@
+package Javaprograms;
+
+import java.sql.SQLOutput;
+
+public class Lab01_Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+}
