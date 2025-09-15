197e8af59d9ac3221909308b03cd146a72882b45
Ganga <gbyadhav@gmail.com>
2025-09-15 14:01:47 +0530

    Printf

---
+++ src/Javaprograms/Ninetable.java
@@ -0,0 +1,12 @@
+package Javaprograms;
+
+public class Ninetable {
+    public static void main(String[] args) {
+        System.out.println("Multiple of 9");
+        for(int i=1;i<=10;i++){
+          int result=9*i;
+            System.out.printf("9*%2d=%3d%n", i,result);
+
+        }
+    }
+}
